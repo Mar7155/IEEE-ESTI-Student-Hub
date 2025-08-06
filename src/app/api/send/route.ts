6a4fd328e505e056e56411266cde724a2d85993b
paulomantilla04 <ma475671@uaeh.edu.mx>
2025-08-06 10:49:08 -0600

    fix: edge runtime added to api route

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,6 +2,8 @@
 import { Resend } from "resend"
 import { NextRequest, NextResponse } from "next/server"
 
+export const runtime = 'edge';
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
